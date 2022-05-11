fcfb1e70f418427bcd7017002cab2019fc93ec74
pavry <lucisofi999@gmail.com>
2022-05-11 13:56:55 +0300

    f

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,24 +12,40 @@ User.create(name: 'Anastasia', email: 'nastya@gmail.com', password: 'alalala', r
 
 
 
-25.times do 
- User.create(
- 	name: Faker::Name.name,
- 	email: Faker::Internet.email,
- 	password: Faker::Internet.password,
- 	role: 'customer',
- 	is_active: [true, false].sample
- )
-end
 
 
 
-25.times do 
- Medicine.create(
-    name: Korvanol,
-    price: Faker::Commerce.price,
-    description: Faker::Lorem.paragraph,
-    age: ['adult', 'young'].sample
- )
-end
+
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+Medicine.create(name: Korvanol, price: 3.75, description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.', age: ['adult', 'young'].sample)
+
+
+# 25.times do 
+#  User.create(
+#  	name: Faker::Name.name,
+#  	email: Faker::Internet.email,
+#  	password: Faker::Internet.password,
+#  	role: 'customer',
+#  	is_active: [true, false].sample
+#  )
+# end
+
+
+
+# 25.times do 
+#  Medicine.create(
+#     name: Korvanol,
+#     price: Faker::Commerce.price,
+#     description: Faker::Lorem.paragraph,
+#     age: ['adult', 'young'].sample
+#  )
+# end
 
